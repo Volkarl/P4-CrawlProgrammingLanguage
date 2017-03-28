e62d0c99a9b84fc020e29226066146330f52608b
silentbessh <silentbessh@gmail.com>
2017-03-28 10:58:49 +0200

    Very early work on typecheck

--- libcompiler/SyntaxTree/Nodes/BlockNode.cs
+++ libcompiler/SyntaxTree/Nodes/BlockNode.cs
@@ -1,17 +1,50 @@
 ﻿using libcompiler.SyntaxTree.Nodes.Internal;
+using libcompiler.TypeChecker;
 
 namespace libcompiler.SyntaxTree.Nodes
 {
     /// <summary>
     /// Represents a block of code, surrounded by Indent .. Dedent
     /// </summary>
-    public class BlockNode : ListNode<CrawlSyntaxNode>
+    public class BlockNode : ListNode<CrawlSyntaxNode>, IScope
     {
+        private BlockScope scopeInfo;
         //TODO: This would be a good place to save scope information.
         
         public BlockNode(CrawlSyntaxNode parent, GreenCrawlSyntaxNode self, int indexInParent)
             : base(parent, self, indexInParent)
         {
+            scopeInfo = new BlockScope(this);
+        }
+        //Checks if the symbol is in this block node scope or the nodes predecessor
+        public TypeInformation[] GetScope(string symbol)
+        {
+            TypeInformation[] typeInformations = scopeInfo.GetScope(symbol);
+            
+            if (typeInformations == null)
+            {
+                IScope scope;
+                scope = FindScopeParent(this);
+                return scope?.GetScope(symbol);
+            }
+            return scopeInfo.GetScope(symbol);
+        }
+        // checks if the parent inherites from IScope, if it inherities it return the node as an IScope,
+        // if the node do not inherites from IScope we check the parents parent.
+        public IScope FindScopeParent(CrawlSyntaxNode node)
+        {
+            if (node.Parent is IScope)
+            {
+                return node.Parent as IScope;
+            }
+            else if (node.Parent == null)
+            {
+                return null;
+            }
+            else
+            {
+                return FindScopeParent(node.Parent);
+            }
         }
     }
 }
---
+++ libcompiler/TypeChecker/BlockScope.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libcompiler.SyntaxTree;
+using libcompiler.SyntaxTree.Nodes;
+
+namespace libcompiler.TypeChecker
+{
+    class BlockScope : IScope
+    {
+        Dictionary<string, TypeInformation[]> scopeDictionary = new Dictionary<string, TypeInformation[]>();
+        //checks if the blockNode is a variable, class or method decleration and adds them to the scopeDictionary    
+        public BlockScope(BlockNode node)
+        {
+            foreach (var child in node)
+            {
+                if(child.Type == NodeType.VariableDecleration)
+                {
+                    VariableDeclerationNode variableNode = (VariableDeclerationNode)child;
+                    foreach (var Decleration in variableNode.Declerations)
+                    {
+                        string name = Decleration.Identifier.Name;
+                        scopeDictionary.Add(name, new TypeInformation[1]);
+                    }
+                }
+                else if(child.Type == NodeType.ClassDecleration)
+                {
+                    ClassDeclerationNode classNode = (ClassDeclerationNode) child;
+                    string name = classNode.Identifier.Value;
+                    scopeDictionary.Add(name, new TypeInformation[1]);
+                }
+                else if(child.Type == NodeType.FunctionDecleration)
+                {
+                    MethodDeclerationNode methodNode = (MethodDeclerationNode) child;
+                    string name = methodNode.Identfier.Name;
+                    scopeDictionary.Add(name, new TypeInformation[1]);
+                }
+            }
+        }
+        //checks if the symbol is in the scopeDictionary else return null
+        public TypeInformation[] GetScope(string symbol)
+        {
+            TypeInformation[] typeArray;
+            if (scopeDictionary.TryGetValue(symbol, out typeArray) == true)
+            {
+                return typeArray;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
