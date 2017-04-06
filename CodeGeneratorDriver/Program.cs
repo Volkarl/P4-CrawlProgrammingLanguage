b6c922c3e9247dcc28991012d399ad0dfa578655
Johannes Elgaard <joha4270@gmail.com>
2017-04-06 22:03:42 +0200

    Now creates greenNodes

---
+++ CodeGeneratorDriver/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace CodeGeneratorDriver
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = Path.Combine(
+                // ReSharper disable once AssignNullToNotNullAttribute
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "SyntaxNodeDefinitions.xml");
+
+            SyntaxGeneration syntaxGeneration = ReadDefinition(path);
+
+            Workspace workspace = new AdhocWorkspace();
+            ;
+            SyntaxGenerator generator = SyntaxGenerator.GetGenerator(workspace, LanguageNames.CSharp);
+
+            foreach (Node node in syntaxGeneration.Node)
+            {
+                SyntaxNode file = Formatter.Format(
+                    GreenNodeGenerator.CreateGreenNode(generator, node, syntaxGeneration.Options),
+                    workspace);
+
+                Console.WriteLine(file);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static SyntaxGeneration ReadDefinition(string path)
+        {
+            var nodes = (SyntaxGeneration)new XmlSerializer(typeof(SyntaxGeneration)).Deserialize(XmlReader.Create(File.OpenRead(path)));
+
+            var dictionary = nodes.Node.ToDictionary(x => x.Name);
+
+            foreach (Node node in nodes.Node)
+            {
+                if(node.BaseClass == null && node.Name == nodes.Options.BaseName) continue;
+
+                node.BaseNode = dictionary[node.BaseClass];
+            }
+
+
+            return nodes;
+        }
+    }
+}
---
+++ CodeGeneratorDriver/SyntaxGeneration.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CodeGeneratorDriver
+{
+    [XmlRoot("syntaxnodes")]
+    public class SyntaxGeneration
+    {
+        [XmlElement("options")]
+        public SyntaxGenerationOptions Options { get; set; }
+
+
+        [XmlArray("nodes")]
+        [XmlArrayItem("node")]
+        public Node[] Node { get; set; }
+
+    }
+
+    public class SyntaxGenerationOptions
+    {
+        [XmlElement("basename")]
+        public string BaseName { get; set; }
+
+        [XmlElement("namespace")]
+        public string NameSpace { get; set; }
+        
+        //s/<([^>]*)>.*/[XmlElement("$1")] public string $1 {get; set;}
+        
+        [XmlElement("parent")]
+        public string Parent {get; set;}
+
+        [XmlElement("createred")]
+        public string CreateRed {get; set;}
+
+        [XmlElement("index")]
+        public string Index {get; set;}
+        
+        [XmlElement("indexinparent")]
+        public string IndexInParent {get; set;}
+
+        [XmlElement("getchildat")]
+        public string GetChildAt { get; set; }
+
+        [XmlElement("invalidchildreplace")]
+        public string InvalidChildReplace { get; set; }
+
+        [XmlElement("with_replaced_child")]
+        public string WithReplacedChild { get; set; }
+
+        [XmlElement("new_child")]
+        public string NewChild { get; set; }
+    }
+
+    public class Node
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("baseclass")]
+        public string BaseClass { get; set; }
+
+        public Node BaseNode { get; set; }
+
+
+        [XmlArray("children")]
+        [XmlArrayItem("child")]
+        public Child[] Children { get; set; } = new Child[0];
+
+        [XmlArray("properties")]
+        [XmlArrayItem("property")]
+        public Property[] Properties { get; set; } = new Property[0];
+
+    }
+
+    public class Property
+    {
+        [XmlElement("type")]
+        public string Type { get; set; }
+
+        [XmlElement("name")]
+        public string Name { get; set; }
+    }
+
+    public class Child
+    {
+        [XmlElement("type")]
+        public string Type { get; set; }
+
+        [XmlElement("name")]
+        public string Name { get; set; }
+    }
+}
