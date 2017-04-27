9e549780492728ef4f4ef1af0b150d466fe7ad24
Johannes Elgaard <joha4270@gmail.com>
2017-04-27 10:25:26 +0200

    Made array types more sane (more restricted compared to normal types

--- libcompiler/TypeSystem/CrawlArrayType.cs
+++ libcompiler/TypeSystem/CrawlArrayType.cs
@@ -21,36 +21,41 @@ namespace libcompiler.TypeSystem
 
         public override bool IsAssignableTo(CrawlType target)
         {
+            //Arrays cannot be cast up or down
+            //Array of Animals, Containing a Tiger, assigned to an array of Dogs?
+            //Array of Dogs, assigned to array of Animal, then a Tiger is inserted?
+
             CrawlArrayType t = target as CrawlArrayType;
-            if (t != null &&
-                t.Dimensions == Dimensions &&
-                t.ArrayElementType.IsAssignableTo(ArrayElementType)
-            )
-                return true;
-            else
-                return false;
+            return t != null &&
+                   t.Dimensions == Dimensions &&
+                   t.ArrayElementType.Equals(ArrayElementType);
         }
 
         public override bool ImplicitlyCastableTo(CrawlType target)
         {
-            CrawlArrayType t = target as CrawlArrayType;
-            if (t != null &&
-                t.Dimensions == Dimensions &&
-                t.ArrayElementType.ImplicitlyCastableTo(ArrayElementType) )
-                return true;
-            else
-                return false;
+            //Casting an entire array in any way dosen't make sense. 
+            //We could try, but it just leads to _weird_ erros when trying
+            //to save something in a casted array.
+
+            //CrawlArrayType t = target as CrawlArrayType;
+            //if (t != null &&
+            //    t.Dimensions == Dimensions &&
+            //    t.ArrayElementType.ImplicitlyCastableTo(ArrayElementType) )
+            //    return true;
+            //else
+            return false;
         }
 
         public override bool CastableTo(CrawlType target)
         {
-            CrawlArrayType t = target as CrawlArrayType;
-            if (t != null &&
-                t.Dimensions == Dimensions &&
-                t.ArrayElementType.CastableTo(ArrayElementType) )
-                return true;
-            else
-                return false;
+
+            //CrawlArrayType t = target as CrawlArrayType;
+            //if (t != null &&
+            //    t.Dimensions == Dimensions &&
+            //    t.ArrayElementType.CastableTo(ArrayElementType) )
+            //    return true;
+            //else
+            return false;
         }
     }
 }
