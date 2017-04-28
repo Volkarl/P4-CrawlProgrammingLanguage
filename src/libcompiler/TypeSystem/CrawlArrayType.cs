4913340199362ce069678d9f5ad74e5c06a79940
RegsaGC <asgerhb@gmail.com>
2017-04-28 14:13:14 +0200

    I have no idea why these weren't commited before. what. the. fuck?

---
+++ src/libcompiler/TypeSystem/CrawlArrayType.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using libcompiler.ExtensionMethods;
+using libcompiler.SyntaxTree;
+
+namespace libcompiler.TypeSystem
+{
+    public class CrawlArrayType : CrawlType
+    {
+        private readonly SingleVariableDeclerationNode _decleration;
+
+        public int Dimensions { get; }
+        public CrawlType ArrayElementType { get; }
+
+        public CrawlArrayType(SingleVariableDeclerationNode declaration, int dimensions, CrawlType arrayElementType)
+            : base(declaration.Identifier.Name, declaration.FindNameSpace().Module)
+        {
+            _decleration = declaration;
+            Dimensions = dimensions;
+            ArrayElementType = arrayElementType;
+        }
+
+        public override bool IsAssignableTo(CrawlType target)
+        {
+            //Arrays cannot be cast up or down
+            //Array of Animals, Containing a Tiger, assigned to an array of Dogs?
+            //Array of Dogs, assigned to array of Animal, then a Tiger is inserted?
+
+            CrawlArrayType t = target as CrawlArrayType;
+            return t != null &&
+                   t.Dimensions == Dimensions &&
+                   t.ArrayElementType.Equals(ArrayElementType);
+        }
+
+        public override bool ImplicitlyCastableTo(CrawlType target)
+        {
+            //Casting an entire array in any way dosen't make sense. 
+            //We could try, but it just leads to _weird_ erros when trying
+            //to save something in a casted array.
+
+            //CrawlArrayType t = target as CrawlArrayType;
+            //if (t != null &&
+            //    t.Dimensions == Dimensions &&
+            //    t.ArrayElementType.ImplicitlyCastableTo(ArrayElementType) )
+            //    return true;
+            //else
+            return false;
+        }
+
+        public override bool CastableTo(CrawlType target)
+        {
+
+            //CrawlArrayType t = target as CrawlArrayType;
+            //if (t != null &&
+            //    t.Dimensions == Dimensions &&
+            //    t.ArrayElementType.CastableTo(ArrayElementType) )
+            //    return true;
+            //else
+            return false;
+        }
+    }
+}
---
+++ src/libcompiler/TypeSystem/CrawlConstructedType.cs
@@ -0,0 +1,89 @@
+﻿using libcompiler.ExtensionMethods;
+using libcompiler.SyntaxTree;
+
+using libcompiler.TypeChecker;
+
+namespace libcompiler.TypeSystem
+{
+    public class CrawlConstructedType : CrawlType, IScope
+    {
+        private readonly ClassTypeDeclerationNode _declaration;
+
+        protected CrawlConstructedType(ClassTypeDeclerationNode declaration)
+            : base(declaration.Identifier.Value, declaration.FindNameSpace().Module)
+        {
+            _declaration = declaration;
+
+            //TODO Will nullref for classes inheriting from $OBJECT
+            Ancestor = (CrawlConstructedType) declaration.FindFirstScope().FindSymbol(declaration.Ancestor.Identifier)[0].Type;
+        }
+
+
+        public CrawlConstructedType Ancestor { get; }
+        //public IReadOnlyList<CrawlType> Interfaces { get; }
+
+
+        public TypeInformation[] FindSymbol(string symbol)
+        {
+            return _declaration.FindSymbolOnlyInThisScope(symbol);
+        }
+
+
+        /// <summary>
+        /// Checks if assigning this to target is legal(according to Cräwl specification).
+        /// </summary>
+        public override bool IsAssignableTo(CrawlType target)
+        {
+            //Are they equal?
+            if (Equals(target))
+                return true;
+
+            //Is this a descendant of target?
+            CrawlConstructedType ancestor = Ancestor;
+            while (ancestor.Identifier != "$OBJECT")
+            {
+                if(ancestor.Equals(target))
+                    return true;
+                ancestor = ancestor.Ancestor;
+            }
+
+            //Is this implicitly castable to target?
+            if(ImplicitlyCastableTo(target))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if an implicit cast from this to target is legal(according to the Cräwl specification).
+        /// </summary>
+        public override bool ImplicitlyCastableTo(CrawlType target)
+        {
+            //TODO Check additional rules for simple types
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if a cast from this to target is legal(according to the Cräwl specification).
+        /// </summary>
+        public override bool CastableTo(CrawlType target)
+        {
+            //TODO Check additional rules for simple types
+
+            //Are they equal?
+            if (Equals(target))
+                return true;
+
+            //Is this a descendant of target?
+            CrawlConstructedType ancestor = Ancestor;
+            while (ancestor.Identifier != "$OBJECT")
+            {
+                if(ancestor.Equals(target))
+                    return true;
+                ancestor = ancestor.Ancestor;
+            }
+
+            return false;
+        }
+    }
+}
